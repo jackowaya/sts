0b5879de1cd71cea7a1f6223260a87714446b3e1
Alan Jackoway <alan.jackoway@gmail.com>
2021-08-08 20:43:59 -0400

    Added Spark SQL example

--- character_win_rates.py
+++ character_win_rates.py
@@ -11,7 +11,14 @@ spark = SparkSession.builder.appName('STS').getOrCreate()
 
 input = spark.read.json(config['DEFAULT']['data_file_directory'] + "/*")
 
+# Pure python version
 (input.select('event.character_chosen', 'event.victory')
     .groupBy('character_chosen', 'victory')
     .agg(count('victory'))
     .orderBy('character_chosen', 'victory')).show()
+
+# Spark SQL Version
+input.createOrReplaceTempView("runs")
+spark.sql("""SELECT event.character_chosen, event.victory, count(*) from runs
+    group by character_chosen, victory
+    order by character_chosen, victory""").show()
